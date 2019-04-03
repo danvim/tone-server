8acec96b57fa7e493537c265f7052bea3a0b9f1b
Daniel Cheung <daniel.2345@hotmail.com>
2019-04-03 13:44:14 +0800

    Update Player.d.ts

--- dist/Game/Player.d.ts
+++ dist/Game/Player.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="peerjs" />
-import Conn = PeerJs.DataConnection;
+import DataConnection = PeerJs.DataConnection;
 import { PackageType } from 'tone-core/dist/lib';
 export declare class Player {
     id: number;
     username: string;
     humanPlayer: boolean;
-    conn: Conn;
-    constructor(conn: Conn);
+    conn: DataConnection;
+    constructor(conn: DataConnection);
     emit(event: PackageType, object: any): void;
 }
